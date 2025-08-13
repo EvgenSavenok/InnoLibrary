a3229e02dc4c682da4892058cac9e7f0cf2fb5c7
EvgenSavenok <eugen.savenok@gmail.com>
2025-08-13 10:23:50 +0300

    Feat: Swagger configuration

---
+++ Infrastructure/Extensions/SwaggerExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Infrastructure.Extensions;
+
+public static class SwaggerExtension
+{
+    public static void ConfigureSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(s =>
+        {
+            s.SwaggerDoc("v1", new OpenApiInfo { Title = "InnoLibrary API", Version = "v1" });
+        });
+    }
+}
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -5,10 +5,28 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.ConfigureUnitOfWork();
+builder.Services.ConfigureSwagger();
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI(s =>
+{
+    s.SwaggerEndpoint("/swagger/v1/swagger.json", "InnoLibrary API");
+});
+
+app.UseRouting();
+
 app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+app.MapControllers();
+app.MapRazorPages();
 
 app.ApplyMigrations();
 
