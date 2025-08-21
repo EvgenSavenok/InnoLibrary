5c7951f25ee4d0d4fef97c0aafa7e1732dbb4fa0
EvgenSavenok <eugen.savenok@gmail.com>
2025-08-21 10:33:36 +0300

    Feat: authors validation

--- Application/UseCases/Booking/Commands/AuthorCommands/CreateAuthor/CreateAuthorCommandHandler.cs
+++ Application/UseCases/Booking/Commands/AuthorCommands/CreateAuthor/CreateAuthorCommandHandler.cs
@@ -1,17 +1,25 @@
 ﻿using Application.Contracts.RepositoryContracts.Booking;
 using Application.MappingProfiles.Booking;
 using Domain.Entities.Booking;
+using FluentValidation;
 using MediatR;
 
 namespace Application.UseCases.Booking.Commands.AuthorCommands.CreateAuthor;
 
 public class CreateAuthorCommandHandler (
-    IUnitOfWork unitOfWork)
+    IUnitOfWork unitOfWork,
+    IValidator<Author> validator)
     : IRequestHandler<CreateAuthorCommand, Author>
 {
     public async Task<Author> Handle(CreateAuthorCommand request, CancellationToken cancellationToken)
     {
         var authorEntity = AuthorMapper.CommandToEntity(request);
+
+        var validationResult = await validator.ValidateAsync(authorEntity, cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(validationResult.Errors);
+        }
         
         await unitOfWork.AuthorRepository.CreateAsync(authorEntity,  cancellationToken);
         
---
+++ Application/Validation/Booking/AuthorValidator.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities.Booking;
+using FluentValidation;
+
+namespace Application.Validation.Booking;
+
+public class AuthorValidator : AbstractValidator<Author>
+{
+    public AuthorValidator()
+    {
+        RuleFor(author => author.FirstName)
+            .NotEmpty().WithMessage("First name is required.")
+            .MaximumLength(100).WithMessage("First name cannot exceed 100 characters.");
+        
+        RuleFor(author => author.LastName)
+            .NotEmpty().WithMessage("Last name is required.")
+            .MaximumLength(100).WithMessage("Last name cannot exceed 100 characters.");
+    }
+}
