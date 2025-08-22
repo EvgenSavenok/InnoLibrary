e5ad8da1fa789d14e1f4c8ac28d5f3096fbc713e
EvgenSavenok <eugen.savenok@gmail.com>
2025-08-22 15:44:42 +0200

    Feat: user registration and authentication

--- Application/MappingProfiles/User/UserMapper.cs
+++ Application/MappingProfiles/User/UserMapper.cs
@@ -1,6 +1,19 @@
-﻿namespace Application.MappingProfiles.User;
+﻿using Application.DTO.User;
 
-public class UserMapper 
+namespace Application.MappingProfiles.User;
+using Domain.Entities.User;
+
+public static class UserMapper 
 {
-   
+   public static User DtoToEntity(UserForRegistrationDto userForRegistrationDto)
+   {
+      return new User
+      {
+         FirstName = userForRegistrationDto.FirstName,
+         LastName = userForRegistrationDto.LastName,
+         Email = userForRegistrationDto.Email,
+         UserName = userForRegistrationDto.UserName,
+         PhoneNumber = userForRegistrationDto.PhoneNumber
+      };
+   }
 }
---
+++ Application/UseCases/User/Commands/UserCommands/Authenticate/AuthenticateUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using Application.DTO.User;
+using MediatR;
+
+namespace Application.UseCases.User.Commands.UserCommands.Authenticate;
+
+public record AuthenticateUserCommand : IRequest<Unit>
+{
+    public UserForAuthenticationDto UserForAuthenticationDto { get; set; }
+}
